0e42f31fc9fbf17a1319c1d57e52cc0a42c60017
SiemensProject <55226350+SiemensProject@users.noreply.github.com>
2019-09-18 13:41:01 +0530

    Add files via upload

---
+++ OSinterfaceGenerator(1226)7.O/SCLMenu/SCLMenu/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace SCLMenu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnBrowseFolder = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnJson = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(328, 82);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.ComboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(203, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Select Key";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.BackColor = System.Drawing.Color.Silver;
+            this.btnSelect.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSelect.Location = new System.Drawing.Point(272, 191);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(95, 34);
+            this.btnSelect.TabIndex = 2;
+            this.btnSelect.Text = "Select File";
+            this.btnSelect.UseVisualStyleBackColor = false;
+            this.btnSelect.Click += new System.EventHandler(this.BtnSelect_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(203, 130);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Select Value";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(328, 127);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 4;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.ComboBox2_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Silver;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(31, 286);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 50);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Create XL File for SCL File";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnBrowseFolder
+            // 
+            this.btnBrowseFolder.BackColor = System.Drawing.Color.Silver;
+            this.btnBrowseFolder.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBrowseFolder.Location = new System.Drawing.Point(416, 235);
+            this.btnBrowseFolder.Name = "btnBrowseFolder";
+            this.btnBrowseFolder.Size = new System.Drawing.Size(101, 39);
+            this.btnBrowseFolder.TabIndex = 7;
+            this.btnBrowseFolder.Text = "Browse Folder";
+            this.btnBrowseFolder.UseVisualStyleBackColor = false;
+            this.btnBrowseFolder.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(145, 248);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(265, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Select Floder Where You Want to store result";
+            // 
+            // btnJson
+            // 
+            this.btnJson.BackColor = System.Drawing.Color.Silver;
+            this.btnJson.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnJson.Location = new System.Drawing.Point(171, 286);
+            this.btnJson.Name = "btnJson";
+            this.btnJson.Size = new System.Drawing.Size(120, 50);
+            this.btnJson.TabIndex = 9;
+            this.btnJson.Text = "Create JSON File For SCL File";
+            this.btnJson.UseVisualStyleBackColor = false;
+            this.btnJson.Click += new System.EventHandler(this.btnJson_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(328, 288);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(105, 46);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Create XL File For AWL File";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(472, 288);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(105, 46);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Create JSON File For AWL File";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(701, 371);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnJson);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnBrowseFolder);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnSelect);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "SCL FILE EXTRACTOR";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button btnBrowseFolder;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnJson;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ OSinterfaceGenerator(1226)7.O/SCLMenu/SCLMenu/Form1.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ConsoleApp2;
+using System.IO;
+using Library;
+
+namespace SCLMenu
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// Capture the input file location which is selected by the user
+        /// </summary>
+        public string InputFileLocation { get; set; }
+
+        /// <summary>
+        /// Capture the output file location where user wants to store the output contents after converting to XLS or JSON formats
+        /// </summary>
+        public string OutputFileLocation { get; set; }
+
+        /// <summary>
+        /// Input file properties key holder
+        /// </summary>
+        public string PropertyKey { get; set; }
+
+        /// <summary>
+        /// Input file properties value holder
+        /// </summary>
+        public string PropertyValue { get; set; }
+
+        /// <summary>
+        /// Holds the collection of property values of the processed input file 
+        /// </summary>
+        public List<EDC> LstEDc = new List<EDC>();
+
+        /// <summary>
+        /// Helper collection for holder the intermediate properties scanned within var_input and var_output
+        /// </summary>
+        public Dictionary<string, List<string>> SCL_InputOutputContents;
+
+    /// <summary>
+    /// contains a property called "name " of input file
+    /// </summary>
+        public string propertyName { get; set; }
+        /// <summary>
+        /// Default Constructor initializing with Combo box
+        /// </summary>
+        /// 
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.Items.Add("S7_m_c");
+            comboBox2.Items.Add("true");
+            comboBox2.Items.Add("false");
+        }
+
+        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            PropertyKey = comboBox1.SelectedItem.ToString();
+        }
+
+        private void BtnSelect_Click(object sender, EventArgs e)
+        {
+            // Prompt user to select input SCL file 
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            openFileDialog1.ShowDialog();
+
+            // Copy absolute path of the SCL file selected by user
+            InputFileLocation = openFileDialog1.FileName;
+            try
+            {
+                // Read SCL file and populate input and output parameter contents for filtering the expected output 
+                SCL_FileExtractor sclFileExtractor = new SCL_FileExtractor();
+                propertyName = sclFileExtractor.GetNamePropertypValue(InputFileLocation);
+                SCL_InputOutputContents = sclFileExtractor.GetInputVarOutputVarContents(InputFileLocation);
+            }
+            catch (FileNotFoundException)
+            {
+                MessageBox.Show("File Not Found !!!");
+            }
+            //Extracts required fields from the input and ouput parameter list
+           
+        }
+
+        private void ComboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //To select value
+            PropertyValue = comboBox2.SelectedItem.ToString();
+        }
+        // When the user selects csv file to be created this method is called
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            PropertyValueExtractorForSCL extractor = new PropertyValueExtractorForSCL();
+            LstEDc = extractor.FindPropertyKey(SCL_InputOutputContents, PropertyKey, PropertyValue);
+            DialogResult result;
+            if (!File.Exists(OutputFileLocation + "\\names1.csv"))
+            {
+                FileWriter writer = new FileWriter();
+                writer.WriteCSV(OutputFileLocation, "names1.csv", LstEDc);
+                MessageBox.Show("Created  " + OutputFileLocation + "\\names1.csv");
+            }
+            else
+            {
+                result = MessageBox.Show("Do you want to create a new file?", "Confirmation", MessageBoxButtons.YesNo);
+
+                if (result == DialogResult.No)
+                {
+
+                    FileWriter writer = new FileWriter();
+                    writer.WriteCSV(OutputFileLocation, "names1.csv", LstEDc);
+                    MessageBox.Show("Created  " + OutputFileLocation + "\\names1.csv");
+                }
+                else
+                {
+                    string f = Microsoft.VisualBasic.Interaction.InputBox("Enter new FileName with required format", "FileName Prompt", "desired default", -1, -1);
+                    FileWriter writer = new FileWriter();
+                    writer.WriteCSV(OutputFileLocation, f, LstEDc);
+                    MessageBox.Show("Created " + OutputFileLocation + "\\" + f);
+
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.folderBrowserDialog1 = new FolderBrowserDialog();
+            folderBrowserDialog1.ShowDialog();
+            OutputFileLocation = folderBrowserDialog1.SelectedPath;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        // When the user selects json file  to be created and input is scl file then this method is called
+        private void btnJson_Click(object sender, EventArgs e)
+        {
+            PropertyValueExtractorForSCL extractor = new PropertyValueExtractorForSCL();
+            LstEDc = extractor.FindPropertyKey(SCL_InputOutputContents, PropertyKey, PropertyValue);
+            string outputFileName = "\\names1.json"; // Final output json file name
+            DialogResult result; // To display message box to the user for prompting if file has been created before in the previous execution
+
+            // Check if the result json file already is present
+            if (!File.Exists(OutputFileLocation + outputFileName))
+            {
+                FileWriter writer = new FileWriter();
+                writer.WriteJson(OutputFileLocation, "names1.json", LstEDc,propertyName);
+                MessageBox.Show("Created " + OutputFileLocation + "\\names1.json");
+            }
+            else
+            {
+                result = MessageBox.Show("Do you want to create a new file?", "Confirmation", MessageBoxButtons.YesNo);
+
+                if (result == DialogResult.No)
+                {
+
+                    FileWriter writer = new FileWriter();
+                    writer.WriteJson(OutputFileLocation, "names1.json", LstEDc, propertyName);
+                    MessageBox.Show("Created  " + OutputFileLocation + "\\names1.json");
+                }
+                else
+                {
+                    string f = Microsoft.VisualBasic.Interaction.InputBox("Enter new FileName with required format", "FileName Prompt", "desired default.json", -1, -1);
+                    FileWriter writer = new FileWriter();
+                    writer.WriteJson(OutputFileLocation, f, LstEDc, propertyName);
+                    MessageBox.Show("Created  " + OutputFileLocation + "\\" + f);
+
+                }
+            }
+
+        }
+        // When the user selects csv file to be created and input is a awl file then this method is called
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            PropertyValueExtractorForAWL extractor = new PropertyValueExtractorForAWL();
+            LstEDc = extractor.FindPropertyKey(SCL_InputOutputContents, PropertyKey, PropertyValue);
+            DialogResult result;
+            if (!File.Exists(OutputFileLocation + "\\names1.csv"))
+            {
+                FileWriter writer = new FileWriter();
+                writer.WriteCSV(OutputFileLocation, "names1.csv", LstEDc);
+                MessageBox.Show("Created  " + OutputFileLocation + "\\names1.csv");
+            }
+            else
+            {
+                result = MessageBox.Show("Do you want to create a new file?", "Confirmation", MessageBoxButtons.YesNo);
+
+                if (result == DialogResult.No)
+                {
+
+                    FileWriter writer = new FileWriter();
+                    writer.WriteCSV(OutputFileLocation, "names1.csv", LstEDc);
+                    MessageBox.Show("Created  " + OutputFileLocation + "\\names1.csv");
+                }
+                else
+                {
+                    string f = Microsoft.VisualBasic.Interaction.InputBox("Enter new FileName with required format", "FileName Prompt", "desired default", -1, -1);
+                    FileWriter writer = new FileWriter();
+                    writer.WriteCSV(OutputFileLocation, f, LstEDc);
+                    MessageBox.Show("Created " + OutputFileLocation + "\\" + f);
+
+                }
+            }
+        }
+        // When the user selects json file to be created and input is a awl file then this method is called
+        private void Button3_Click_1(object sender, EventArgs e)
+        {
+            PropertyValueExtractorForAWL extractor = new PropertyValueExtractorForAWL();
+            LstEDc = extractor.FindPropertyKey(SCL_InputOutputContents, PropertyKey, PropertyValue);
+            string outputFileName = "\\names1.json"; // Final output json file name
+            DialogResult result; // To display message box to the user for prompting if file has been created before in the previous execution
+
+            // Check if the result json file already is present
+            if (!File.Exists(OutputFileLocation + outputFileName))
+            {
+                FileWriter writer = new FileWriter();
+                writer.WriteJson(OutputFileLocation, "names1.json", LstEDc, propertyName);
+                MessageBox.Show("Created " + OutputFileLocation + "\\names1.json");
+            }
+            else
+            {
+                result = MessageBox.Show("Do you want to create a new file?", "Confirmation", MessageBoxButtons.YesNo);
+
+                if (result == DialogResult.No)
+                {
+
+                    FileWriter writer = new FileWriter();
+                    writer.WriteJson(OutputFileLocation, "names1.json", LstEDc, propertyName);
+                    MessageBox.Show("Created  " + OutputFileLocation + "\\names1.json");
+                }
+                else
+                {
+                    string f = Microsoft.VisualBasic.Interaction.InputBox("Enter new FileName with required format", "FileName Prompt", "desired default.json", -1, -1);
+                    FileWriter writer = new FileWriter();
+                    writer.WriteJson(OutputFileLocation, f, LstEDc, propertyName);
+                    MessageBox.Show("Created  " + OutputFileLocation + "\\" + f);
+
+                }
+            }
+
+        }
+    }
+}
